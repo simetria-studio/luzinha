09f470977a3b34457d02f3dada01fe9fef48bcea
tiago <tiago.ezidio99@gmail.com>
2021-08-01 22:24:54 -0300

    asdsad

--- resources/views/layouts/home/index.blade.php
+++ resources/views/layouts/home/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.main')
 
 @section('content')
-
+    <div class="container">
+        <div>
+            <img src="{{ url('assets/img/bom-dia.png') }}" alt="">
+        </div>
+    </div>
 @endsection
