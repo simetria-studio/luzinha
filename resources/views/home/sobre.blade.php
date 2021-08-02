555a08051f3b7cdfe78fc5e16486fee24066e8c8
tiago <tiago.ezidio99@gmail.com>
2021-08-01 23:14:57 -0300

    asdsad

--- resources/views/home/sobre.blade.php
+++ resources/views/home/sobre.blade.php
@@ -1,8 +1,15 @@
 @extends('layouts.main')
 
 @section('content')
-
     <div class="titulo">
         <p>SOBRE A AUTORA</p>
+    </div>
+    <div class="foto-lia">
+        <img src="{{ url('assets/img/foto_lia.png') }}" alt="">
+    </div>
+    <div class="container">
+        <div class="texto">
+            <p>Noa está desde cedo envolvida no mundo das artes. Aos 6 anos ingressou no ballet clássico pela Royal Academy of Dancing. Aos 10 começou a ter aulas de violino. Poesia, essa acompanha-a desde sempre. Cantora e compositora, dos álbuns "As coisas boas" de 2015 e "Cicatriz" de 2018, prepara-se para apresentar a terceira obra. Mestre em Literatura Portuguesa pela Faculdade de Letras da Universidade do Porto. Foi mãe há 2 anos de uma menina chamada Luz. Esta nova fase inspirou-a para escrever uma coleção de 6 livros infantis de carácter pedagógico chamada "O Mundo da Luzinha".</p>
+        </div>
     </div>
 @endsection
