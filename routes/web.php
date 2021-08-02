5f615505503e49df2aacaf7a6d673d6728d46f53
felipe cruz <felipephplow@gmail.com>
2021-08-01 22:15:44 -0300

    dsad

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('layout', [SiteController::class, 'layout']);
+
+Route::get('/', [SiteController::class, 'index']);
+
