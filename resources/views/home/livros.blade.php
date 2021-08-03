b24b361ff01ece839b7311976042acdc7f34c2df
tiago <tiago.ezidio99@gmail.com>
2021-08-03 15:49:48 -0300

    PRONTOOOOOO

--- resources/views/home/livros.blade.php
+++ resources/views/home/livros.blade.php
@@ -4,23 +4,17 @@
     <div class="titulo">
         <p>LIVROS</p>
     </div>
-    <div class="container livros">
-        <div class="bom-dia">
-           <a href="{{ url('livro-2') }}"> <img src="{{ url('assets/img/bom-dia.png') }}" alt=""></a>
-        </div>
+    <div class="container">
+        <div class="livros">
+            <div class="bom-dia">
+                <a href="{{ url('livro-2') }}"> <img src="{{ url('assets/img/bom-dia.png') }}" alt=""></a>
+            </div>
 
-        <div class="hora-da-papa mt-3">
-            <img src="{{ url('assets/img/hora-papa.png') }}" alt="">
-
-        <div class="hora-da-papa">
-            <a href="{{ url('livro-1') }}"><img src="{{ url('assets/img/hora-papa.png') }}" alt=""></a>
-
-
-        <div class="hora-da-papa">
-            <a href="{{ url('livro-1') }}"><img src="{{ url('assets/img/hora-papa.png') }}" alt=""></a>
+            <div class="hora-da-papa mt-3">
+                <img src="{{ url('assets/img/hora-papa.png') }}" alt="">
+            </div>
 
 
         </div>
-
     </div>
 @endsection
