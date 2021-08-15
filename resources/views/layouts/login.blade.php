ffa85fbd0f18c1f7f2a757641c79b62b1da4b4da
tiago <tiago.ezidio99@gmail.com>
2021-08-14 21:29:43 -0300

    asdsad

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -13,9 +13,9 @@
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
     <link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css"
         href="https://cdn.jsdelivr.net/gh/greghub/green-audio-player/dist/css/green-audio-player.min.css">
     <link rel="stylesheet" href="{{ url('assets/css/login.min.css') }}">
@@ -33,18 +33,16 @@
 
     </div>
     <div class="fechar">
-        <a href="/home" class="btn btn-fechar">X</a>
+        <a href="/home" class="btn btn-fechar"><i class="fas fa-times-circle"></i></a>
     </div>
     <main>
         @yield('content')
     </main>
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
-        < script src = "https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js" >
-    </script>
-    </script>
+    <script src="https://kit.fontawesome.com/e67788a73f.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
     <script src="https://cdn.jsdelivr.net/gh/greghub/green-audio-player/dist/js/green-audio-player.min.js"></script>
     <script src="{{ url('/assets/js/script.js') }}"></script>
 
