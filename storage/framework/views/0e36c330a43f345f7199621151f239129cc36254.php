ddc81e0ded8ae801d1de7c85747d5982262e91e0
felipe cruz <felipephplow@gmail.com>
2021-08-25 14:51:50 -0300

    aa

--- storage/framework/views/0e36c330a43f345f7199621151f239129cc36254.php
+++ storage/framework/views/0e36c330a43f345f7199621151f239129cc36254.php
@@ -1,6 +1,4 @@
-@extends('layouts.main')
-
-@section('content')
+<?php $__env->startSection('content'); ?>
     <div class="titulo">
         <p>CONTATOS</p>
     </div>
@@ -8,17 +6,17 @@
         <div class="d-flex">
             <div class="icone whatsapp">
                 <a href="https://api.whatsapp.com/send?phone=351910123364">
-                    <img src="{{ url('assets/img/icones/whatsapp.png') }}" alt="">
+                    <img src="<?php echo e(url('assets/img/icones/whatsapp.png')); ?>" alt="">
                 </a>
             </div>
             <div class="icone telefone">
                 <a href="tel:++351910123364">
-                    <img src="{{ url('assets/img/icones/phone.png') }}" alt="">
+                    <img src="<?php echo e(url('assets/img/icones/phone.png')); ?>" alt="">
                 </a>
             </div>
             <div class="icone e-mail">
                 <a href="mailto:contato@omundodaluzinha.com">
-                    <img src="{{ url('assets/img/icones/email.png') }}" alt="">
+                    <img src="<?php echo e(url('assets/img/icones/email.png')); ?>" alt="">
                 </a>
             </div>
         </div>
@@ -30,19 +28,21 @@
         <div class="d-flex position">
             <div class="icone">
                 <a href="https://www.instagram.com/omundodaluzinha">
-                    <img src="{{ url('assets/img/redes/instagram.png') }}" alt="">
+                    <img src="<?php echo e(url('assets/img/redes/instagram.png')); ?>" alt="">
                 </a>
             </div>
             <div class="icone">
                 <a href="https://www.facebook.com/O-Mundo-da-Luzinha-107425637753380">
-                    <img src="{{ url('assets/img/redes/facebook.png') }}" alt="">
+                    <img src="<?php echo e(url('assets/img/redes/facebook.png')); ?>" alt="">
                 </a>
             </div>
             <div class="icone">
                 <a href="https://www.youtube.com/user/NoaViolinPlayer1">
-                    <img src="{{ url('assets/img/redes/youtube.png') }}" alt="">
+                    <img src="<?php echo e(url('assets/img/redes/youtube.png')); ?>" alt="">
                 </a>
             </div>
         </div>
     </div>
-@endsection
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\luzinha\resources\views/home/contatos.blade.php ENDPATH**/ ?>
