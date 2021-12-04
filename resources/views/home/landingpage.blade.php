7d7bbbae8bc97d73a8293ba2009553c7f2ee3908
felipe cruz <felipephplow@gmail.com>
2021-12-03 23:30:47 -0300

    d

--- resources/views/home/landingpage.blade.php
+++ resources/views/home/landingpage.blade.php
@@ -8,30 +8,49 @@
     {{-- fonts --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200;300;400;500;600;700;800&display=swap"
         rel="stylesheet">
+    <script src="https://kit.fontawesome.com/0ab2bcde1c.js" crossorigin="anonymous"></script>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css"
         href="https://cdn.jsdelivr.net/gh/greghub/green-audio-player/dist/css/green-audio-player.min.css">
     <link rel="stylesheet" href="{{ url('assets/css/landing.min.css') }}">
-    <title>Luzinha</title>
+    <title>CJM - REFORMAS</title>
 </head>
 
 <body>
-    <div class="container">
-        <div class="seila">
-            <div class="lp">
-                <div class="logo">
-                    <img src="{{ url('assets/lp/logo.png') }}" alt="">
-                </div>
-                <div class="playstore">
-                    <a href="https://play.google.com/store/apps/details?id=com.mundo.luzinhaweb">
-                        <img src="{{ url('assets/lp/playstore.png') }}" alt="">
+    <div class="full">
+        <div class="back">
+
+
+        </div>
+        <div class="circle">
+            <img src="{{ url('assets/lp/clodoaldo.png') }}" alt="">
+        </div>
+        <div class="text-center name">
+            <h4>CLODOALDO LIMA</h4>
+        </div>
+        <div class="redes d-flex">
+            <div class="d-flex">
+                <div>
+                    <a href="tel:+554198854401">
+                        <div class="icon">
+                            <span><i class="fas fa-phone"></i></span>
+                        </div>
                     </a>
                 </div>
+            </div>
+            <div class="d-flex">
+                <div>
+                    <a
+                      target="_blank"  href="https://api.whatsapp.com/send?phone=5541998854401&text=Ol%C3%A1%20gostaria%20de%20solicitar%20um%20or%C3%A7amento">
+                        <div class="icon">
+                            <span><i class="fab fa-whatsapp"></i></span>
+                        </div>
+                    </a>
+                </div>
+
             </div>
         </div>
     </div>
